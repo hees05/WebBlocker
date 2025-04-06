0d98e5b316d84be844be54327fde766cd2d1c0b5
hees05 <hees05@hees>
2025-04-06 03:16:36 +0100

    I've added the logic to display the reason on the final screen, along with the whole page.

--- warning.js
+++ warning.js
@@ -7,5 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
         chrome.runtime.sendMessage({ type: "close_and_open_google" });
       });
     }
+      // Show the keyword that triggered the block
+    const urlParams = new URLSearchParams(window.location.search);
+    const reason = urlParams.get("reason");
+
+    if (reason) {
+        const reasonDiv = document.getElementById("reason");
+        if (reasonDiv) {
+        reasonDiv.textContent = `You were denied access due to the keyword: “${reason}”.`;
+        }
+    }
   });
   
